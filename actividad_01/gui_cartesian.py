881a79ce36babbaba8afb5f7cbe506e8e2d228be
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2019-01-30 14:21:26 -0500

    4 mvp
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>

--- actividad_01/gui_cartesian.py
+++ actividad_01/gui_cartesian.py
@@ -11,20 +11,14 @@ def onclick(event):
 
 def setupPlane(points=[]):
     plt.plot(range(300), linewidth=1.0)
-    plt.show()
     plt.grid()
 
     fig.canvas.mpl_connect('button_press_event', onclick)
     ax.legend()
-    ax.grid(True)
+    drawPoint(points)
     plt.show()
 
-    print("points from setup", points)
-    drawPoint(points)
-
-
 def drawPoint(points=[]):
-    print(points)
     color=['red', 'green', 'blue']
     scale = 100
     for point in points:
@@ -32,6 +26,6 @@ def drawPoint(points=[]):
         y = point[1]
         print(x, " valor ", y, " con respecto a ", point )
 
-        ax.scatter(x, y, c=color[0], s=scale, alpha=0.3, edgecolors='face')
+        ax.scatter(int(x), int(y), c=color[0], s=scale, alpha=0.3, edgecolors='face')
 
     plt.show()
