98ffa706775ad68efe209bdfe5d237b6e1dcc03b
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2019-01-30 11:51:11 -0500

    Adds main
    select file
    read the csv file
    show the plane
    
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>

---
+++ actividad_01/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+
+import gui_cartesian as plane
+import gui_loadfile as files
+
+if __name__ == "__main__":
+
+    print(" starting ")
+
+    file_selected = files.selectFile()
+    points = files.getListFromCSV(file_selected)
+    plane.setupPlane(points)
+    file_selected.quite()
+
