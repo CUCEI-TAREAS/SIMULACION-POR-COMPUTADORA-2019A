0778cf48b39c5651d5a4fa178c0968623b1c5403
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2019-03-06 03:30:15 -0600

    Add distance_between_points for Points class
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>

--- actividad_03/points.py
+++ actividad_03/points.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env
 
+import math
+
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, lis=None, x=0, y=0):
+
+       	if lis.any():
+            self.x = int(x)
+            self.y = int(y)
+       	    return
+
+        self.x = int(lis[0])
+        self.y = int(lis[1])
+
+	# TODO implement magic method to receive list of points
+    def __eq__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+	
 
 
+def distance_between_points(pointA, pointB):
+    """ return the euclidean distance between 2 points """
+
+    return math.sqrt(math.pow(( pointA.x - pointB.x), 2) + math.pow(( pointA.y - pointB.y), 2))
