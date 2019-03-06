41ef1fa5d623b2cc98fa4f6c94fc6eaaf9ba0b5d
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2019-03-06 14:49:32 -0600

    Improve circle drawing
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>

--- actividad_03/circle.py
+++ actividad_03/circle.py
@@ -12,26 +12,22 @@ def bresenham_circle(center, curve):
         curve: point over the curve use as radius
         return: list of points representing the circle
     """
-    radius = int(pts.distance_between_points(center, curve))
+    radius = pts.distance_between_points(center, curve)
 
-    f = int(1 - radius)
+    f = 1 - radius
     ddf_x = 1
-    ddf_y = int(-2 * radius)
+    ddf_y = -2 * radius
 
     x = 0
     y = radius
 
 
     points = list()
-	
-    points.append(pts.Point(center.x, center.y + radius))
-    points.append(pts.Point(center.x, center.y - radius))
-    points.append(pts.Point(center.x + radius, center.y))
-    points.append(pts.Point(center.x - radius, center.y))
 
-    points.append(pts.Point(center.x, center.y - radius))
-    points.append(pts.Point(center.x + radius, center.y))
-    points.append(pts.Point(center.x - radius, center.y))
+    points.append(pts.Point(center.x, round(center.y + radius)))
+    points.append(pts.Point(center.x, round(center.y - radius)))
+    points.append(pts.Point(round(center.x + radius), center.y))
+    points.append(pts.Point(round(center.x + radius), center.y))
 
     while x < y:
         if f >= 0: 
@@ -41,16 +37,16 @@ def bresenham_circle(center, curve):
         x += 1
         ddf_x += 2
         f += ddf_x    
-        points.append(pts.Point(center.x + x, center.y + y))
-        points.append(pts.Point(center.x - x, center.y + y))
-        points.append(pts.Point(center.x + x, center.y - y))
-        points.append(pts.Point(center.x - x, center.y - y))
-        points.append(pts.Point(center.x + y, center.y + x))
-        points.append(pts.Point(center.x - y, center.y + x))
-        points.append(pts.Point(center.x + y, center.y - x))
-        points.append(pts.Point(center.x - y, center.y - x))
+        points.append(pts.Point(round(center.x + x), round(center.y + y)))
+        points.append(pts.Point(round(center.x - x), round(center.y + y)))
+        points.append(pts.Point(round(center.x + x), round(center.y - y)))
+        points.append(pts.Point(round(center.x - x), round(center.y - y)))
+        points.append(pts.Point(round(center.x + y), round(center.y + x)))
+        points.append(pts.Point(round(center.x - y), round(center.y + x)))
+        points.append(pts.Point(round(center.x + y), round(center.y - x)))
+        points.append(pts.Point(round(center.x - y), round(center.y - x)))
 
-	
+
     return points
 
 
@@ -78,23 +74,23 @@ def dda_circle(center, curve):
         x2 = center.x - x
         x3 = x2 * x2
 
-        y2 = abs( int( radius_2 - x3 ) )
-        y = int(math.sqrt( y2 ))
+        y2 = abs( radius_2 - x3 )
+        y = math.sqrt( y2 )
 
         points.append(pts.Point(x, center.y + y ))
         points.append(pts.Point(x, center.y - y ))
-	
+
     # for y 
     for x in range( int(center.y - radius), int(center.y + radius), 1 ):
 
         x2 = center.y - x
         x3 = x2 * x2
 
-        y2 = abs( int( radius_2 - x3 ) )
-        y = int(math.sqrt( y2 ))
+        y2 = abs( radius_2 - x3 )
+        y = math.sqrt( y2 )
 
-        points.append(pts.Point(center.x + y, x ))
-        points.append(pts.Point(center.x - y, x ))
+        points.append(pts.Point(round(center.x + y), x ))
+        points.append(pts.Point(round(center.x - y), x ))
 
     return points
 
